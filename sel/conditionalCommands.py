1373ecc6553852f901bf7b30a98908c56e2a63ab
Rishabh Desai <desai.rishab@gmail.com>
2022-02-25 21:05:30 +0530

    conditional commands updated

--- sel/conditionalCommands.py
+++ sel/conditionalCommands.py
@@ -1,8 +1,9 @@
 # ref videos  https://www.youtube.com/watch?v=o3tYiyE_OXE
 """
-command to learn :
-driver.back()
-driver.forward()
+command to learn:
+is_dispayed()
+is_enabled()
+is_selected()
 """
 
 import time
@@ -13,21 +14,12 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path="C:\drivers\chromedriver_win32\chromedriver.exe")
 
-driver.get("https://www.pavantestingtools.com/")
-time.sleep(5)
-print(driver.title)
-
 driver.get("https://crystalemr.freshdesk.com/support/home")
+# ele = driver.find_element_by_name("term")
+ele= driver.find_element(by=By.NAME, value="term")
 time.sleep(5)
-print(driver. title)
+print(ele.is_displayed())  # return True/False based on element status
+print(ele.is_enabled())
 
-driver.back()
-time.sleep(5)
-print(driver.title)
 
-driver.forward()
-time.sleep(5)
 
-print(driver.title)
-
-driver.quit()
